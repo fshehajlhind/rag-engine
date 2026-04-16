49d58562486a133d1146b8e6b5d714ff8f3b08be
fshehajlhind <fridi.shehaj@lhind.dlh.de>
2026-04-16 09:26:35 +0200

    added docstring, removed debug prints

--- app/api/routes.py
+++ app/api/routes.py
@@ -22,7 +22,15 @@ def health():
 def get_articles(page: int = Query(1, ge=1),
                  page_size: int = Query(10, ge=1, le=50),
                  db: Session = Depends(get_db)):
-    """Returns full list of articles."""
+    """Returns full list of articles.
+     Args:
+        page (int): Page number >= 1.
+        page_size (int): Number of articles to return per page.
+        db: Database session.
+    Returns:
+          total_count (int): Total number of articles.
+          articles (List[ArticleOut]): List of articles.
+    """
     articles = db.query(Article).offset((page - 1) * page_size).limit(page_size).all()
     total_count = db.query(Article).count()
     logging.info(f"Fetched %d articles", total_count)
@@ -34,7 +42,15 @@ def get_articles(page: int = Query(1, ge=1),
 
 @router.get("/articles/{article_id}", response_model=ArticleOut)
 def get_article_by_id(article_id: str, db: Session = Depends(get_db)):
-    """Returns article by id"""
+    """Returns article by id
+    Args:
+        article_id: uuid of the article.
+        db: Database session.
+    Returns:
+        Article: Matching article from the database.
+     Raises:
+        HTTPException: If the article is not found.
+    """
     article = db.query(Article).filter(Article.uuid == article_id).first()
     logging.info(f"Fetched article with id: {article_id} , url: {article.url}, title: {article.title}")
     if not article:
@@ -44,7 +60,12 @@ def get_article_by_id(article_id: str, db: Session = Depends(get_db)):
 
 @router.get("/stats")
 def get_stats(db: Session = Depends(get_db)):
-    """Returns total articles by source."""
+    """Returns total articles for each source.
+     Args:
+        db: Database session.
+     Returns:
+         dict: Dictionary of article counts for each source together with teh source.
+    """
     grouped_articles = db.query(Article.source, func.count(Article.uuid)).group_by(Article.source).all()
     logging.info("Grouped articles by source ", grouped_articles)
     return {
